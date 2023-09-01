1133488ac1f6aa9840d77500978557d9f0fcf83e
Mehmet Simsek <simsekmehmet00@gmail.com>
2023-09-01 21:35:44 +0200

    initial commit

---
+++ server/src/main/java/com/task/server/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.task.server.repository;
+
+import com.task.server.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
